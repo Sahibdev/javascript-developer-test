f4c5daac076d79bea51a15db11491525ca17767f
Sahibdev <sahibdev.mann@yahoo.com>
2022-07-03 07:34:46 +1000

    feat(arine-quote): passes all unit test

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,29 @@
 const { httpGet } = require('./mock-http-interface');
 
+const OK = 200;
+const SUCCESS = 'Arnie Quote';
+const FAILURE = 'FAILURE';
+const NO_STATUS = 'Response Error - Cannot find Status';
+
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  return Promise.all(
+    urls.map(async (url) => {
+      try {
+        const response = await httpGet(url);
+        if (response?.status) {
+          const { status } = response;
+          const key = status === OK ? SUCCESS : FAILURE;
+
+          const { message } = JSON.parse(response.body);
+          return { [key]: message };
+        } else {
+          throw new Error(NO_STATUS);
+        }
+      } catch (err) {
+        return err;
+      }
+    })
+  );
 };
 
 module.exports = {
